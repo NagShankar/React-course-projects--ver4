7ee1d80f07e87e864820a3ed9b9d6d8437a8e41d
NagShankar <nag.shankar6@gmail.com>
2020-03-13 14:38:48 +0530

    class property syntax

--- src/app.js
+++ src/app.js
@@ -9,3 +9,30 @@ document.getElementById('app')
 )
 
 
+//class OldSyntax{
+//    constructor(){
+//        this.name='nag';
+//        this.getGreeting = this.getGreeting.bind(this);//to bind this to the meethod
+//    }
+//    getGreeting(){
+//        return `Hi, i am ${this.name}`
+//    }
+//}
+//const oldSyn = new OldSyntax();
+//console.log(oldSyn);
+//const fetchGreeting=oldSyn.getGreeting;
+//console.log(fetchGreeting());//this gets broken if not bound in the contructor
+//
+
+//------------------
+
+//class NewSyntax{
+//    name='marc';
+//getGreeting = () => {
+//     return `Hi, i am ${this.name}`
+//}
+//}
+//const NewSyn = new NewSyntax();
+//console.log(NewSyn);
+//const fetchGreeting2=NewSyn.getGreeting;
+//console.log(fetchGreeting2());//here cuz of arrow functions which do not have this bound, and takes the scope of function, here the class, as its a class properties syntax, binds this automatically
